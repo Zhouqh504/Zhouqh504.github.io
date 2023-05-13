29ca17b6dc339adac80f9dafc38cd6759995158c
Zhouqh504 <130747012+Zhouqh504@users.noreply.github.com>
2023-05-13 11:08:02 +0800

    Add files via upload

---
+++ Rectangular Manhattan Plot/Rectangular Manhattan Plot/install.R
@@ -0,0 +1,4 @@
+if (!require('shiny')) install.packages('shiny')
+if (!require('CMplot')) install.packages("CMplot", version = "4.3.1")
+if (!require('colourpicker')) install.packages('colourpicker')
+if (!require('htmltools')) install.packages('htmltools')
