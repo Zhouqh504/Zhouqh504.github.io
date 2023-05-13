fbfeec93d5470c92336a761350b39fc6c613c388
Zhouqh504 <130747012+Zhouqh504@users.noreply.github.com>
2023-05-13 11:13:53 +0800

    Delete Rectangular Manhattan Plot directory

--- Rectangular Manhattan Plot/install.R
+++
@@ -1,4 +0,0 @@
-if (!require('shiny')) install.packages('shiny')
-if (!require('CMplot')) install.packages("CMplot", version = "4.3.1")
-if (!require('colourpicker')) install.packages('colourpicker')
-if (!require('htmltools')) install.packages('htmltools')
